779449f05beca36abc3ed2e69a0596b970ece109
muttta <93360763+muttta@users.noreply.github.com>
2021-12-06 14:59:51 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'homes#new'
+  get '/homes', to: 'homes#new'
+  post '/homes/result', to: 'home#result'
 end
