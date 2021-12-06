9558c0cb44890b265df48a03028b1198b7eccd4f
muttta <93360763+muttta@users.noreply.github.com>
2021-12-06 15:12:14 +0900

    homeモデル作成

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,7 @@
 class Home < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :feel
+
+  validates :category_id, :feel_id, presence: true
 end
