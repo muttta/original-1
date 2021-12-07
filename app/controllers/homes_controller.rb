5043b52d32cba34ddd9fb4ebd39a003f9e18f5eb
muttta <93360763+muttta@users.noreply.github.com>
2021-12-07 15:36:12 +0900

    表示画面作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,8 +4,16 @@ class HomesController < ApplicationController
     @home = Home.new
   end
 
-  def result
-    home = params[:home].permit(:category, :feel)
-    @home = Home.new(HomesController)
+  def result 
+    @home = Home.new(home_params)
+      rand_num = rand(4)+1
+      @words = Word.find([rand_num])
+      
+  end
+
+  private
+
+  def home_params
+    params.require(:home).permit(:category_id, :feel_id)
   end
 end
